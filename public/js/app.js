c2c01e4dc63aa9a56816203337e7081cccaf7e42
kibethh <kibethh@gmail.com>
2021-05-13 05:21:15 +0100

    Remove unnecessar console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,16 +1,3 @@
-console.log("Client side javascript is loaded!!.");
-
-//fetch("http://localhost:3000/weather?address=!").then((response) => {
-//    response.json().then((data) => {
-//        if (data.error) {
-//            console.log(data.error);
-//        } else {
-//            console.log(data.forecast);
-//            console.log(data.location);
-//        }
-//    });
-//});
-
 const weatherForm = document.querySelector("form");
 const search = document.querySelector("input");
 const messageOne = document.querySelector("#message-1");
